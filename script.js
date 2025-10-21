2e2347dd8f1e3a73c640f9a6b8c86b2878ad34d5
mubarokatulatikaatika-ship-it <mubarokatulatikaatika@gmail.com>
2025-10-21 13:27:09 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tombolsapa = Docummet. getElementById('sapaButton');
+// 2. Tambahkan 'event Listener' untuk aksi 'click'
+tombolsapa.addEventlistener('click', function() {
+    // 3. Jalankan kode ini ketika tombol di-klik 
+  alert('Halo! Terimakasih sudah berkunjung!');
+  {);
+                            
